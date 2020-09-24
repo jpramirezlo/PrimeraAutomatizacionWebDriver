a42694c27e49c55bc2856406b74eb2b4d46112db
jpramirezlo <jramirez2803@gmail.com>
2020-09-23 19:48:01 -0500

    pasos del primer caso de prueba

--- src/test/java/automationtesting/aperturapagina/TestAbrirPagina.java
+++ src/test/java/automationtesting/aperturapagina/TestAbrirPagina.java
@@ -2,9 +2,9 @@ package automationtesting.aperturapagina;
 
 import automationtesting.Utilidades;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -23,8 +23,15 @@ public class TestAbrirPagina {
 
         driver.manage().window().maximize();
         driver.get("http://practice.automationtesting.in/");
-
+        driver.findElement(By.id("menu-item-50")).click();
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("username")).sendKeys("pruebaregistro10@correo.com");
+        driver.findElement(By.id("password")).click();
+        driver.findElement(By.id("password")).sendKeys("Prueba1+.,");
+        driver.findElement(By.name("login")).click();
         Utilidades.esperar(10);
+
+
     }
 
     @After
